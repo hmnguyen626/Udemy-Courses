ebe4c30a6815fa4a63d0c7f0673a644cbdea290a
Hieu Nguyen <31426975+HieumongousNguyen@users.noreply.github.com>
2018-05-30 03:30:38 -0700

    Completed auto-layout calculator challenge

--- iOS/Stack View/Stack View/ViewController.swift
+++ iOS/Stack View/Stack View/ViewController.swift
@@ -10,16 +10,170 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // Outlets
+    @IBOutlet weak var valueLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        updateUI(value: input)
     }
+    
+    // Variables
+    var input : Character? = nil
+    var calculatorOperator : Character? = nil
+    var value1 : String? = nil
+    var value2 : String? = nil
+    var negative : Bool = false
 
+    // Updates our current label ui using Strings.
+    // If it is nil, then set valueLabel's text property as an empty string.
+    func updateUI(value: Character?){
+        guard let value = value else {
+            valueLabel.text = ""
+            return
+        }
+        
+        valueLabel.text?.insert(value, at: (valueLabel.text?.endIndex)!)
+    }
+    
+    func makeAlert(){
+        // Alert
+        let alert = UIAlertController(title: "Error", message: "Something has gone terribly wrong.", preferredStyle: .alert)
+        let alertAction = UIAlertAction(title: "Reset", style: .default) { (UIAlertAction) in
+            self.resetCalculator()
+        }
+        
+        alert.addAction(alertAction)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+    // CalculateValues Closure
+    var calculateValues: (String?, String?, Character?) -> String? = { x, y, logic in
+        switch logic {
+        case "+":
+            guard let x = x, let y = y else {
+                return nil
+            }
+
+            return String(Float(x)! + Float(y)!)
+        case "-":
+            guard let x = x, let y = y else {
+                return nil
+            }
+            
+            return String(Float(x)! - Float(y)!)
+        case "X":
+            guard let x = x, let y = y else {
+                return nil
+            }
+            
+            return String(Float(x)! * Float(y)!)
+        case "/":
+            guard let x = x, let y = y else {
+                return nil
+            }
+            
+            return String(Float(x)! / Float(y)!)
+        case "%":
+            guard let x = x, let y = y else {
+                return nil
+            }
+            
+            // '%' is unavailable: For floating point numbers use truncatingRemainder instead
+            // Source: https://stackoverflow.com/questions/40495301/what-does-is-unavailable-use-truncatingremainder-instead-mean
+            return String(Float(x)!.truncatingRemainder(dividingBy: Float(y)!))
+        default:
+            print("Something wrong happen with calculateValues switch logic.")
+            return "Broken default listed"
+        }
+    }
+    
+    // Resets all variable to initial conditions
+    func resetCalculator(){
+        input = nil
+        calculatorOperator = nil
+        value1 = nil
+        value2 = nil
+        updateUI(value: input)
+    }
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        switch sender.tag {
+        case 0...9:
+            updateUI(value: Character("\(sender.tag)"))
+        case 10:
+            resetCalculator()
+        case 11:
+            // Sets negative value
+            if(negative){
+                valueLabel.text?.remove(at: (valueLabel.text?.startIndex)!)
+                negative = false
+            } else {
+                valueLabel.text?.insert("-", at: (valueLabel.text?.startIndex)!)
+                negative = true
+            }
+        case 12:
+            // Store first value for later use and set operator
+            value1 = valueLabel.text
+            calculatorOperator = "%"
+            
+            updateUI(value: nil)
+        case 13:
+            // Store first value for later use and set operator
+            value1 = valueLabel.text
+            calculatorOperator = "/"
+            
+            updateUI(value: nil)
+        case 14:
+            // Store first value for later use and set operator
+            value1 = valueLabel.text
+            calculatorOperator = "X"
+            
+            updateUI(value: nil)
+        case 15:
+            // Store first value for later use and set operator
+            value1 = valueLabel.text
+            calculatorOperator = "-"
+            
+            updateUI(value: nil)
+        case 16:
+            // Store first value for later use and set operator
+            value1 = valueLabel.text
+            calculatorOperator = "+"
+            
+            updateUI(value: nil)
+        case 17:
+            // If our string contains a "." already, then break.
+            if (valueLabel.text?.contains("."))!{
+                break
+            } else {
+                updateUI(value: ".")
+            }
+        case 18:
+            // Store second value for use in "=" operator with value 1
+            value2 = valueLabel.text
+            
+            // Validate that optional is not nil, else present an alert to user
+            if let value1 = value1, let value2 = value2 {
+                // Presents an alert to user if there is an error in logic, else assign valueLabel.text to correct result.
+                if calculateValues(value1, value2, calculatorOperator) == "Broken logic" {
+                    makeAlert()
+                } else {
+                    valueLabel.text = calculateValues(value1, value2, calculatorOperator)
+                }
+            } else {
+                makeAlert()
+            }
+        default:
+            print("Invalid")
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
